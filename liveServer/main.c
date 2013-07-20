99e9032084a5158818d47adbf5001a5eec767dee
7imbrook <timbrook480@gmail.com>
2013-07-20 16:53:09 -0400

    Initial Commit

---
+++ liveServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  liveServer
+//
+//  Created by Michael Timbrook on 7/20/13.
+//  Copyright (c) 2013 Michael Timbrook. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
