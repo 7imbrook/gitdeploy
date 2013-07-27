88e5b0ca52e1d2b49ab55f805a53484141a04332
7imbrook <timbrook480@gmail.com>
2013-07-27 17:34:39 -0400

    Did something with main. Bad commit ik

--- src/main.c
+++ src/main.c
@@ -65,26 +65,12 @@ git_reference *_newestTagRef = NULL;
 
 int checkTags(const char *name, git_oid *oid, void *payload) {
     
-    char *strFormat = calloc(80, sizeof(char));
-    sprintf(strFormat, "/tmp/live/.git/%s", name);
-    FILE *sha = fopen(strFormat, "r");
+    git_commit *commit = NULL;
     
-    unsigned char hash[41];
-    for(int i = 0; i < 40; i++)
-        hash[i] = getc(sha);
-    hash[40] = '\0';
-
-    printf("%s -> %s\n", hash, name);
+    git_commit_lookup(&commit, _repo, oid);
     
-    // git_oid tagoid;
-    // git_oid_fromraw(&tagoid, hash);
-    
-    // git_commit *commit = NULL;
-    
-    // git_commit_lookup(&commit, _repo, &tagoid);
-    
-    // const char *message = git_commit_message(commit);
-    // printf("%s", message);
+    const char *message = git_commit_message(commit);
+    printf("%s", message);
     
     return 0;
 }
@@ -92,15 +78,13 @@ int checkTags(const char *name, git_oid *oid, void *payload) {
 int main(int argc, const char * argv[])
 {
     
-    system("rm -rf /tmp/live"); // <<<<< TESTING CODE, NOT FINAL >>>>> //
-    
     int error;
-    if ((error = git_clone(&_repo, "/Users/michael/Projects/liveServer", "/tmp/live", NULL))) {
-        printf("Failed to clone with error %d\n", error);
+    if ((error = git_repository_open(&_repo, "/tmp/live"))) {
+        printf("Failed to open with error number %d\n", error);
         return 1;
     }
     
-    printf("Successfully cloned repo to /tmp/live\n");
+    printf("Successfully opened repo in /tmp/live\n");
     print_branch_name(_repo);
     printf("Printing last commit:\n  --> ");
     print_head_commit_message(_repo);
@@ -109,8 +93,6 @@ int main(int argc, const char * argv[])
     printf("Reading tags...\n");
     git_tag_foreach_cb testcb = &checkTags;
     git_tag_foreach(_repo, testcb,  NULL);
-    
-    
     
     // Freedom!
     git_repository_free(_repo);
