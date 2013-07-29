0aea3dae445edc604d1a72b619f5ac0a5807f31d
7imbrook <timbrook480@gmail.com>
2013-07-28 20:25:05 -0400

    Added regex to check is a tag is deployable

--- src/main.c
+++ src/main.c
@@ -10,9 +10,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define false 0
 #define true 1
+
+const char *repoLocation = "/Users/michael/Projects/myTestRepo/";
 
 // Prints repo in form "On branch $(branch_name)"
 static void print_branch_name(git_repository *repo) {
@@ -61,16 +64,25 @@ static void print_head_commit_message(git_repository *repo) {
 }
 
 git_repository *_repo = NULL;
-git_reference *_newestTagRef = NULL;
+const char *deployed_tag;
 
+// Get the latest deployable tag, deployable is are vX.X.0 tags
 int checkTags(const char *name, git_oid *oid, void *payload) {
     
-    git_commit *commit = NULL;
+    const char *tag = &name[10];
     
-    git_commit_lookup(&commit, _repo, oid);
+    regex_t regex;
+    int reti;
+    char messBuff[100];
     
-    const char *message = git_commit_message(commit);
-    printf("%s", message);
+    if ((reti = regcomp(&regex, "v[0-9].[0-9].0", 0))) {
+        printf("Regex failed\n");
+    }
+    
+    if (!regexec(&regex, tag, 0, NULL, 0)) {
+        printf("%s is deployable\n", tag);
+    }
+    
     
     return 0;
 }
@@ -79,12 +91,12 @@ int main(int argc, const char * argv[])
 {
     
     int error;
-    if ((error = git_repository_open(&_repo, "/tmp/live"))) {
+    if ((error = git_repository_open(&_repo, repoLocation))) {
         printf("Failed to open with error number %d\n", error);
         return 1;
     }
     
-    printf("Successfully opened repo in /tmp/live\n");
+    printf("Successfully opened repo\n");
     print_branch_name(_repo);
     printf("Printing last commit:\n  --> ");
     print_head_commit_message(_repo);
